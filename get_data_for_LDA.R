212d66df5c439e2012b0eef834a621a04c0c1c42
Erica Christensen <erica.christensen@weecology.org>
2017-10-20 15:23:51 -0400

    updated function for reading in ant data

--- get_data_for_LDA.R
+++ get_data_for_LDA.R
@@ -7,29 +7,26 @@ library(reshape2)
 #' Processes ant census data so it can be used for LDA analysis
 #' This script parallels rodent_data_for_LDA.R in Extreme-events-LDA project
 #'
-#' @param yr_first first year of data desired
-#' @param yr_last last year of data desired
 #' @param selected_plots vector of plot numbers to be included
 #' 
 #' @return Table of colony counts species per period
 #' 
 #' @example
-#' a_table = ant_colony_table(1977,2009,c(1,2,5,6,7,9,11,14,15,16,18,21,22))
+#' a_table = ant_colony_table(c(1,2,5,6,7,9,11,14,15,16,18,21,22))
 #'
-ant_colony_table = function(yr_first,yr_last,selected_plots=1:24) {
+ant_colony_table = function(selected_plots=1:24) {
   
   # retrieve current version of ant data
   ants = read.csv(text=getURL("https://raw.githubusercontent.com/weecology/PortalData/master/Ants/Portal_ant_colony.csv"),
-                  na.strings=c("",-99), stringsAsFactors = FALSE)
+                  stringsAsFactors = FALSE)
   
   # extract desired data by year and plot; remove unkn species and 'sole xylo' which was not censused in every year; remove 1981 because rare species 
-# not censused
-  A = filter(ants, Year>=yr_first, Year<=yr_last,
-             Plot %in% selected_plots) %>% select(Year,Plot,Species,Colonies)
+  # not censused
+  A = filter(ants, plot %in% selected_plots) %>% select(year,plot,species,colonies,flag)
   
   # sum number of colonies per year
-  A_colonies = aggregate(A$Colonies,by=list(Year = A$Year, Species = A$Species),FUN=sum)
-  A_table = reshape(A_colonies, idvar = 'Year', timevar = 'Species', direction = 'wide')
+  A_colonies = aggregate(A$colonies,by=list(year = A$year, species = A$species),FUN=sum)
+  A_table = reshape(A_colonies, idvar = 'year', timevar = 'species', direction = 'wide')
   
   # change column names
   #names(A_table)[-1] %>% strsplit('\\.') %>% unlist()
@@ -39,6 +36,28 @@ ant_colony_table = function(yr_first,yr_last,selected_plots=1:24) {
   #fill in NA with 0
   A_table[is.na(A_table)] = 0
   row.names(A_table) = A_table$Year
+  
+  return(A_table[,-1])
+  
+  # retrieve current version of ant data
+  ants = read.csv(text=getURL("https://raw.githubusercontent.com/weecology/PortalData/master/Ants/Portal_ant_bait.csv"),
+                  stringsAsFactors = FALSE)
+  
+  # extract desired data by year and plot; remove unkn species 
+  A = filter(ants, plot %in% selected_plots) %>% select(year,plot,species,abundance)
+  
+  # sum abundance per year
+  A_abund = aggregate(A$abundance,by=list(year = A$year, species = A$species),FUN=sum)
+  A_table = reshape(A_abund, idvar = 'year', timevar = 'species', direction = 'wide')
+  
+  # change column names
+  #names(A_table)[-1] %>% strsplit('\\.') %>% unlist()
+  
+  #order rows by year
+  A_table = A_table[order(A_table$year),]
+  #fill in NA with 0
+  A_table[is.na(A_table)] = 0
+  row.names(A_table) = A_table$year
   
   return(A_table[,-1])
 }
