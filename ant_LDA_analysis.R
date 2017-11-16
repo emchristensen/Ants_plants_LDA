61b398a59161219e56237688065910915e7007c0
Erica Christensen <erica.christensen@weecology.org>
2017-11-16 11:53:14 -0500

    update ant_LDA_analysis.R because it wasn't working

--- ant_LDA_analysis.R
+++ ant_LDA_analysis.R
@@ -13,43 +13,51 @@ library(topicmodels)
 library(multipanelfigure)
 
 
-source('get_data_for_LDA.r')
+#source('get_data_for_LDA.r')
 
 source('../Extreme-events-LDA/AIC_model_selection.R')
-source('../Extreme-events-LDA/LDA_figure_scripts.R')
-source('changepointmodel.r')
-source('LDA-distance.R')
+source('LDA_figure_scripts_antsplants.R')
+source('../Extreme-events-LDA/changepointmodel.r')
+source('../Extreme-events-LDA/LDA-distance.R')
 
 # ===================================================================
-# prepare rodent data
-dat = ant_colony_table(1977,2009,c(1,2,5,6,7,9,11,14,15,16,18,21,22))
-dat_openings = ant_opening_table(1977,2009,c(1,2,5,6,7,9,11,14,15,16,18,21,22))
-dat_abund = ant_bait_table(1977,2009,c(1,2,5,6,7,9,11,14,15,16,18,21,22))
+# prepare ant data
+#dat = ant_colony_table(1977,2009,c(1,2,5,6,7,9,11,14,15,16,18,21,22))
+#dat_openings = ant_opening_table(1977,2009,c(1,2,5,6,7,9,11,14,15,16,18,21,22))
+#dat_abund = ant_bait_table(1977,2009,c(1,2,5,6,7,9,11,14,15,16,18,21,22))
+
+dat = read.csv('Ant_colony_numstakes.csv',as.is=T)
+dat$date = substr(dat$index,1,4) %>% as.numeric()
+dat = dat %>% select(-index)
+yrdat = aggregate(.~date,data = dat,sum)
+antdat = yrdat %>% select(-date)
 
 # ===================================
 # model parameters:
 topic_min = 2
 topic_max = 9
-nspp=length(dat)
+nspp=length(antdat)
 # ==================================================================
 # select number of topics
 
 # Fit a bunch of LDA models with different seeds
 # Only use every other seed because consecutive seeds give identical results (!?)
-seeds = 2*seq(200)
+seeds = 2*seq(50)
 
 # repeat aic calculation with a bunch of different seeds to test robustness of the analysis
-best_ntopic_abund = repeat_VEM(dat_abund,
+best_ntopic_abund = repeat_VEM(antdat,
                          seeds,
                          topic_min=2,
                          topic_max=10)
 
 # plot histogram of how many seeds chose how many topics
-hist(best_ntopic[,1],breaks=c(0.5,1.5,2.5,3.5,4.5,5.5,6.5,7.5,8.5,9.5),xlab='best # of topics', main='')
+hist(best_ntopic_abund$k,breaks=c(0.5,1.5,2.5,3.5,4.5,5.5,6.5,7.5,8.5,9.5,10,10.5),xlab='best # of topics', main='')
+
+# 5 topics seems to be best, but 6, 7, 8 are also well-represented
 
 # ============================================================
-ntopics=2
-ldamodel = LDA(dat,ntopics, control = list(seed = 1),method='VEM')
+ntopics=5
+ldamodel = LDA(antdat,ntopics, control = list(seed = 1),method='VEM')
 beta1 = community_composition(ldamodel)
 plot_community_composition(beta1)
-plot_component_communities(ldamodel,ntopics,as.Date(row.names(dat),format='%Y'))
+plot_component_communities(ldamodel,ntopics,as.Date(as.character(yrdat$date),format='%Y'))
