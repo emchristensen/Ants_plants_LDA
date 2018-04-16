5c5a7f0a316ab0cee281747530a0a8d5a4b0e262
Erica Christensen <erica.christensen@weecology.org>
2018-04-16 16:35:28 -0400

    movement_models.R uses movement_model_functions.R to do community distance analysis on Portal data

--- movement_models.R
+++ movement_models.R
@@ -16,32 +16,62 @@
 # droughts (Bestelmeyer et al. 2011a)."
 
 
-library(vegan)
 
-source('../portalr/R/PlantData.R')
-source('../portalr/R/plant_data_processing.R')
+#source('../portalr/R/PlantData.R')
+#source('../portalr/R/plant_data_processing.R')
 source('create_data_tables.R')
+source('movement_model_functions.R')
 
+
+# do analysis with rodent data
+rodentsummer = rodent_dist_summers(selected_plots=c(2,11,14,22),dist_method = 'bray')
+
+
+rodentsummer$nYrs <- rodentsummer$year - min(rodentsummer$year)
+# gradual linear dynamics
+nomad.Mod <- gradual_linear_dyn(rodentsummer)
+rodentsummer$nomad_gnls <- unlist(fitted(nomad.Mod))
+# reversible dynamics
+migr.Mod <- reversible_dyn(rodentsummer)
+rodentsummer$mig_gnls <- unlist(fitted(migr.Mod))
+# stable dynamics
+stab.Mod = stable_dyn(rodentsummer)
+rodentsummer$stab_gnls <- unlist(fitted(stab.Mod))
+# abrupt shift
+disp.Mod = abrupt_dyn(rodentsummer)
+rodentsummer$abrupt_gnls <- unlist(fitted(disp.Mod))
+
+# look at data with 4 different model fits
+plot_fits(rodentsummer)
+
+# obtain aic from each model
+quad_aic = data.frame(plot.id=plots[k],
+                      aic_nomad=AIC(nomad.Mod),
+                      #aic_migr=AIC(migr.Mod),
+                      aic_stab=AIC(stab.Mod),
+                      aic_disp=AIC(disp.Mod))
+quad_aic
+
+
+# -----------------------------------------------------------------
 # portal plant data
 
 # control plots for everything 1977-present
 #select_plots = c(11,14)
 # control plots w.r.t ants and rodents 1977-2015
-#select_plots = c(2,11,14,22)
-# test with single plot
-select_plots = 22
+select_plots = c(2,11,14,22)
 
-summertable = summer_annual_byplot(select_plots)
-wintertable = winter_annual_byplot(select_plots)
+#summertable = summer_annual_byplot(select_plots)
+wintertable = winter_annual_byplot(select_plots,dist_method='bray')
+
+#write.csv(wintertable,'winterannuals.csv',row.names=F)
 
 # Used Bray-Curtis (except for one dataset was just presence/absence so they used Sorensen)
 # I think they did plot-level aggregate with Portal
 # Distance from each sample relative to initial
 
-summerdist = vegdist(summertable[,-c(1:4)],method='bray')
-summer_ts = summerdist[1:length(summertable$year-1)]
-plot(unique(summertable$year),summer_ts,type='b')
+#summerdist = vegdist(summertable[,-c(1:4)],method='bray')
+#summer_ts = summerdist[1:length(summertable$year-1)]
+#plot(unique(summertable$year),summer_ts,type='b')
 
-winterdist = vegdist(wintertable[,-c(1:4)],method='bray')
-winter_ts = winterdist[1:length(wintertable$year-1)]
-plot(unique(wintertable$year),winter_ts,type='b')
+
